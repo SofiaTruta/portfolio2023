344dee5d195ce583027587bd2e55af43782a1019
sofiatruta <sofiatruta@gmail.com>
2023-11-30 13:46:41 +0000

    layout about technologies

--- components/about.jsx
+++ components/about.jsx
@@ -1,9 +1,12 @@
 const About = () => {
-    return ( 
-        <div>
-            About
-        </div>
+    return (
+            <div className="py-20 px-10">
+                <h1 className="text-2xl font-bold mb-5">about</h1>
+                <p className="text-base leading-relaxed mb-3">After 8 years in healthcare and 2 years in healthTech, in my last role I had the opportunity to contribute to product development of a web app for internal use. </p>
+                <p className="text-base leading-relaxed mb-3">This sparked my curiosity and since then I decided to pursue software engineering! </p>
+                <p className="text-base leading-relaxed">Recently completed General Assembly's Software Engineering bootcamp, I have learned the fundamentals of Full Stack software development (JavaScript, React, Vue.js, Express.js, Python, Django).</p>
+            </div>
      );
 }
- 
+
 export default About;
--- components/hero/hero.jsx
+++ components/hero/hero.jsx
@@ -1,21 +1,37 @@
-import Icon from '@mdi/react'
-import { mdiEmail } from '@mdi/js'
-
 const Hero = () => {
     return (
         <>
-            <div className='top-0 flex max-h-screen w-1/2 flex-col justify-between py-24'>
-                <p className='text-6xl'>Sofia Truta</p>
-                <p className='text-4xl'>Junior Software Developer</p>
-                <div className='flex'>
-                    <i className="devicon-google-plain"></i>
-                    <i className="devicon-github-original"></i>
-                    <i className="devicon-linkedin-plain"></i>
+            <div className="bg-gray-700 w-1/2 pt-10 sticky top-0 max-h-screen">
+                <div className="m-10">
+                    <p className='text-6xl mb-1'>Sofia Truta</p>
+                    <p className='text-3xl mb-3'>Junior Software Developer</p>
+                    <div className='flex'>
+                        <button className="rounded-full border border-slate-500 p-2">Email</button>
+                        <button className="rounded-full border border-slate-500 p-2">GitHub</button>
+                        <button className="rounded-full border border-slate-500 p-2">LinkedIn</button>
+                    </div>
                 </div>
-
-                <nav>
-                    <div className='vertical-line'></div>
-                 
+                <nav className="flex m-20">
+                    <ul>
+                        <li className="py-2">
+                            <a href="">about</a>
+                        </li>
+                        <li className="py-2">
+                            <a href="">technologies</a>
+                        </li>
+                        <li className="py-2">
+                            <a href="">projects</a>
+                        </li>
+                        <li className="py-2">
+                            <a href="">experience</a>
+                        </li>
+                        <li className="py-2">
+                            <a href="">learning now</a>
+                        </li>
+                        <li className="py-2">
+                            <a href="">interests</a>
+                        </li>
+                    </ul>
                 </nav>
             </div>
         </>
